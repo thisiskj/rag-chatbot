81ccbdf516ad571347b2a5a999963471904649eb
Kevin Jadunandan <kevin.jadunandan@gmail.com>
2025-02-01 18:56:34 -0500

    Update CORS

--- web-backend/main.py
+++ web-backend/main.py
@@ -46,6 +46,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://frontend-production-79aa.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
