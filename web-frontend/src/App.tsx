8f83ed2e9cf09267a525b4462a62e5968741b717
Kevin Jadunandan <kevin.jadunandan@gmail.com>
2025-02-01 18:48:38 -0500

    Env and loading spinner

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -1,33 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm";
+import LoadingSpinner from './LoadingSpinner';
 
 function App() {
     const [messages, setMessages] = useState([])
-
-    useEffect(() => {
-
-    }, [])
+    const [loading, setLoading] = useState(false)
 
     const onSubmit = (e) => {
         e.preventDefault()
         setMessages([])
+        setLoading(true)
+
         const formData = new FormData(e.target);
-        const formJson = Object.fromEntries(formData.entries());
         // @ts-ignore-error
         const queryString = new URLSearchParams(formData).toString()
         
-        console.log(formData.toString())
-        console.log(formJson)
-        console.log(queryString)
-
-        
-        const evtSource = new EventSource(`http://127.0.0.1:8000/search?${queryString}`, {
+        const evtSource = new EventSource(`${import.meta.env.VITE_API_URL}/search?${queryString}`, {
             withCredentials: true,
         });
 
         evtSource.addEventListener("res", (event) => {
+            setLoading(false)
             setMessages((prevState) => {
                 return [...prevState, event.data]
             })
@@ -43,23 +38,29 @@ function App() {
     }
 
     return (
-        <div className="flex justify-center">
+        <div className="my-8 flex justify-center">
             <div className="w-xl">
                 <div className="text-2xl font-bold mb-4">Ask Django</div>
                 <form onSubmit={onSubmit}>
                     <div className="mb-4">
-                        <input type="text" className="w-full text-sm text-slate-600 bg-white border border-slate-300 appearance-none rounded-lg px-3.5 py-2.5 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100" id="query" name="q" placeholder="Ask me about Django" required />
+                        <input type="text" className="w-full text-sm text-slate-600 bg-white border border-slate-300 appearance-none rounded-lg px-3.5 py-2.5 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100" id="query" name="q" placeholder="Ask me anything about Django" required />
                     </div>
                     <button className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150 group">
-                        Submit <span className="tracking-normal text-indigo-300 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+                        Go
                     </button>
                 </form>
-
-                <div className="mt-10 text-left">
-                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                        {messages.join('')}
-                    </ReactMarkdown>
-                </div>
+                {loading ? (
+                    <div className="text-center my-4">
+                        <LoadingSpinner />
+                    </div>
+                ): (
+                    <div className="mt-10 text-left">
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                            {messages.join('')}
+                        </ReactMarkdown>
+                    </div>
+                )}
+                
             </div>
         </div>
         
